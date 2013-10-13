526d19d5a42c029f64805854f8c33c0b68c4b2c5
Juan Amorin <juan_amorin@hotmail.com>
2013-10-12 23:18:45 -0430

    init

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -47,5 +47,86 @@ object Application extends Controller {
        Ok(views.html.index()).withNewSession
     }
   }
+  
+  def showPerfil(id :Long) = Action
+  {
+      friendId = id;
+      Redirect("/perfilPag")
+  }
+  
+  def showPerfilPag = Action
+  {
+      var userDao: UsuarioDAO = DAOFabrica.getUsuarioDAO;
+      var userSelected = userDao.findUserById(friendId).getOrElse{null}
+      Ok(views.html.perfil(userSelected))
+  }
+  
+  
+  def showAlbumes(op :Integer) = Action
+  {
+      Redirect("/albumesPag")
+  } 
+  
+  def showAlbumesPag = Action
+  {   
+      var albumDao: AlbumDAO = DAOFabrica.getAlbumDAO;
+      var albumes = albumDao.findAlbumsByUser(currentUser.getId) 
+      Ok(views.html.albumes(albumes,currentUser,albumDao))
+  }
+  
+  def showAmigos(id :Long) = Action
+  {
+      friendId = id;
+      Redirect("/amigosPag")
+  } 
+  
+  def showAmigosPag = Action
+  {
+      var userDao: UsuarioDAO = DAOFabrica.getUsuarioDAO;
+      var userSelected = userDao.findUserById(friendId).getOrElse{null}
+      userSelected.setAmistades(userDao.findFriendsByUser(userSelected.getId))
+      Ok(views.html.amigos(userSelected))
+  }
+  
+  def showAlbumContent(op :Integer) = Action
+  {
+      Redirect("/albumContentPag")
+  } 
+  
+   def showAlbumContentPag = Action
+   {
+      Ok(views.html.carrusel()) 
+   }
+   
+   
+   def updateRegisteredUser(option: Int,attr: String,id: Long) = Action
+   {
+         var userDao: UsuarioDAO = DAOFabrica.getUsuarioDAO;
+         
+         if(option == 1)
+         {
+             userDao.updateUserNames(attr,id)
+             currentUser.setNombres(attr)
+         }
+         if(option == 2)
+         {
+             userDao.updateUserLastnames(attr,id)
+             currentUser.setApellidos(attr)
+         }
+         if(option == 3)
+         {
+             userDao.updateUserNickname(attr,id)
+             currentUser.setUsername(attr)
+         }
+         if(option == 4)
+         {
+             userDao.updateUserEmail(attr,id)
+             currentUser.setEmail(attr)
+         }
+         
+         Ok("true")
+   }
+  
+  
 
 }
