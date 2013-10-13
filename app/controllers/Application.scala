b6f9db6c6784d75a25d87eba5ebf6b6f62b36c87
Juan Amorin <juan_amorin@hotmail.com>
2013-10-12 22:23:17 -0430

    init

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,25 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-object Application extends Controller {
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
+import routes.javascript._
 
+import views._
+import models._
+
+import anorm._
+import anorm.SqlParser._
+
+
+object Application extends Controller {
+  
+  var currentUser: Usuario = null;
+  var friendId: Long = 0;
+  
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
 
 }
